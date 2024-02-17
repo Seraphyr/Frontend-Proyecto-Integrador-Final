13e94a738222048e3791568a4ef27822e5a96573
Alex D. <t.docheff@gmail.com>
2024-02-16 21:48:07 -0300

    creacion componente login y selector universal *

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div>
+            <div class="login-container">
+                <div><img src="../logo-large(1).png" alt="" /></div>
+                <form id="login-form">
+                    <button type="button" className='btn'>Registrarse Gratis</button>
+                            <button type="button" className='btn-transparente'>Iniciar Sesión</button>
+                        </form>
+                    </div>
+            </div>
+
+            )
+}

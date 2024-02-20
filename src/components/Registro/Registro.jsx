a5f67b9b4e0f9575c77b2336a263e2afc8e0f26e
Mateo <mateo.nicoloff25@gmail.com>
2024-02-20 20:07:26 -0300

    primer pantalla de registro

--- src/components/Registro/Registro.jsx
+++ src/components/Registro/Registro.jsx
@@ -1,9 +1,30 @@
 import Header from "../Header/Header";
+import "./registro.css";
 
 export default function Login() {
-    return (
-        <>      
-        <Header titulo={"Crear Cuenta"}/>
-        </>
-    )
+  return (
+    <>
+      <Header titulo={"Crear Cuenta"} />
+      <div className="conteiner__registro">
+        <h1 className="registro__titulo">¿Cuál es tu correo eléctronico?</h1>
+        <div className="registro__form">
+          <p className="registro__parrafo">Correo electónico:</p>
+          <form action="">
+            <input
+              type="text"
+              className="registro__input"
+              name="registroEmail"
+              id="registroEmail"
+            />
+            <p className="registro__form__parrafo">
+              Deberás poder confirmarlo luego.
+            </p>
+            <div className="registro__boton">
+              <button>Continuar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }

7c2b8d40dff55d4078559cfb753453fabef3f1a8
Mateo <mateo.nicoloff25@gmail.com>
2024-02-26 19:05:25 -0300

    Ajustes Front

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,16 +7,18 @@ export default function Login() {
     return (
         <>
             <div className="login-container">
-                <div ><img src="/logo-large.png" className="logo" alt="" /></div>
+                <div className='logo__titulo' ><img src="/logo-large.png" className="logo" alt="" />
                 <h2 className='container-titulo'>Música a medida.</h2>
+                </div>
                 <div>
                     <img src="/gif-landing.gif" className="gif" />
                 </div>
                 
-            </div>
+           
             <div className='btn-container'>
-            <Link to={'/registro'}><button type="button" className='btn'>Registrarse Gratis</button></Link>
+            <Link to={'/registro'}><button type="button" className='btn__login'>Registrarse Gratis</button></Link>
                     <Link to={'/iniciosesion'}><button  type="button" className='btn-transparente'>Iniciar Sesión</button></Link>
+                </div>
                 </div>
         </>
 

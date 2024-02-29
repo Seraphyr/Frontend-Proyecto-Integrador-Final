24e5f600e1d29f6250e53b0f12141abeba73d302
Alex D <t.docheff@gmail.com>
2024-02-29 20:25:55 -0300

    funcion match de cupido musical

--- api/useArtistas.jsx
+++ api/useArtistas.jsx
@@ -4,18 +4,26 @@ import { useEffect, useState } from "react";
 
 export default function useArtistas(){
     const [listaArtistas, setListaArtistas] = useState([])
- 
+    const [artistasRandom, setArtistasRandom] = useState([])
+
     const verArtistas = async () => {
         const response = await fetch("http://localhost:3000/verArtistas")
         const data = await response.json();
 
         setListaArtistas(data)
     }
-   
+    const verArtistasRandom = async () => {
+        const response = await fetch("http://localhost:3000/artistasRandom")
+        const data = await response.json();
+        console.log(data);
+        setArtistasRandom(data)
+    }
 
     return{
         verArtistas,
-        listaArtistas
+        listaArtistas,
+        artistasRandom,
+        verArtistasRandom
     }
     
 }

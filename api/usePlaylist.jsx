4777ea295affe70717dcc1e43635a11cdfdd2dc0
Mateo <mateo.nicoloff25@gmail.com>
2024-02-29 20:34:32 -0300

    cambios MC

--- api/usePlaylist.jsx
+++ api/usePlaylist.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function usePlaylist(){
 
     const [playlist, setPlaylist] = useState([])
+    const [playlistMC, setPlaylistMC] = useState([])
 
     const cupidoMusical = async (listaArtistas) => {
         const response = await fetch("http://localhost:3000/crearCupidoMusical",{
@@ -23,9 +24,28 @@ export default function usePlaylist(){
     }
 
 
+    const musicaContextual = async (listaGeneros) => {
+        const response = await fetch("http://localhost:3000/crearMusicaContextual",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                token: Cookies.get("token"),
+            },
+
+            body: JSON.stringify({
+                listaGeneros
+            })        
+        })
+        const data = await response.json()
+        setPlaylistMC(data)
+    }
+
+
 return{
     cupidoMusical,
-    playlist
+    playlist,
+    musicaContextual,
+    playlistMC
 }
 
 }
